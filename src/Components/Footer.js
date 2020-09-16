7d0e13ad0360512aea8b766693f7e89d989c8147
ahnaf22 <austcse14@gmail.com>
2020-09-16 19:24:44 +0600

    updated info and some css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -24,7 +24,7 @@ class Footer extends Component {
 
             <ul className="copyright">
               <li>&copy; Ahnaf Ahmad</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Design by <a title="Styleshout" href="#">Styleshout</a></li>
             </ul>
 
           </div>
